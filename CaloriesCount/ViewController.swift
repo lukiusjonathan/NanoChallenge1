06d0d805d771fec382eded93e285ef4356b9d5ef
Lukius Jonathan <lukiusjonathan@gmail.com>
2020-03-03 19:48:58 +0700

    Initial Commit

---
+++ CaloriesCount/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CaloriesCount
+//
+//  Created by Lukius Jonathan on 03/03/20.
+//  Copyright © 2020 Lukius Jonathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
